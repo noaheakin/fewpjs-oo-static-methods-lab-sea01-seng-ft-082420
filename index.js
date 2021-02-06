95545c1c35606804a97fbac4029996dc72b1c7e8
noaheakin <noaheakin@gmail.com>
2021-02-05 20:21:26 -0800

    commit tree 34f3697bb5857e6820b28457aebff432b7e5a038

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string) {
+    let arr = string.split(' ')
+    let lowercase = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let title = []
+    for (let i = 0; i < arr.length; i++) {
+      if (i === 0 || !lowercase.includes(arr[i])) {
+        title.push(this.capitalize(arr[i]))
+      } else {
+        title.push(arr[i])
+      }
+    }
+    return title.join(' ')
+  }
 }
